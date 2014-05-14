fd03ec8204ef254bf172b6c944442428484eac83
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-14 10:02:15 +0800

    初始化代码库

---
+++ MakeFile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p src/main/java/com/bodejidi/
+
+mkdir -p src/main/webapp/WEB-INF/
+
+mkdir -p target/classes/
+
+mkdir lib/
+
+mkdir sql/
+
+mkdir -p src/test/java/com/com/bodejidi/
