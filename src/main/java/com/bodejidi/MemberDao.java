506999e71dcf1c0aaed3ea11e4c87e7f240360fc
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-16 16:40:07 +0800

    增加了添加到personalInformation表中的信息语句

--- src/main/java/com/bodejidi/MemberDao.java
+++ src/main/java/com/bodejidi/MemberDao.java
@@ -4,23 +4,43 @@ public class MemberDao
 {
 	public void userSave(Member member)
 	{
-		String sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
+	
+		String Email = member.getEmail();
+		String username = member.getUsername();
+		String password = member.getPassword();
+		String firstName = member.getFirstName();
+		String lastName = member.getLastName();
+		String address = member.getAddress();
+
+		String tb_username_sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
+		String tb_personalInformation_sql = "INSERT INTO tb_personalInformation (username, password, firstName, lastName,  address) VALUE(?, ?, ?, ?, ?)";
 		DatabaseService bs = DatabaseService.newInstance();
 		try
 		{
-			bs.prepare(sql)
-				.setString(member.getUsername())
-				.setString(member.getEmail())
+			bs.prepare(tb_username_sql)
+				.setString(username)
+				.setString(Email)
 				.execute();
-			System.out.println("SQL: " + sql);
+			System.out.println("SQL: " + tb_username_sql );
+			bs.prepare(tb_personalInformation_sql)
+				.setString(username)
+				.setString(password)
+				.setString(firstName)
+				.setString(lastName)
+				.setString(address)
+				.execute();
+			System.out.println("SQL: " + tb_personalInformation_sql );
+		
 		}
 		catch(Exception e)
 		{
-			
+		
+		
 		}
-
-	
-
+		finally
+		{
+			bs.close();
+		}
 	}
 
 }
