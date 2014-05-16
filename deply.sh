283e3e8f3ecc0011ddd64e4ee068e43e89058052
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-16 14:11:34 +0800

    将关闭脚本移入，在打开tomcat之前

--- deply.sh
+++ deply.sh
@@ -4,4 +4,5 @@ cp -r ./src/main/webapp/* $CATALINA_HOME/webapps/practice/
 cp -r ./target/* $CATALINA_HOME/webapps/practice/WEB-INF/
 cp -r lib $CATALINA_HOME/webapps/practice/WEB-INF/
 
+$CATALINA_HOME/bin/shutdown.sh
 $CATALINA_HOME/bin/startup.sh
