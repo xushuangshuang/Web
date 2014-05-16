8fb922bc6d881faf2d49223cf63471797d9dbe4a
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-16 12:19:10 +0800

    修改了脚本，加入了lib文件夹

--- deply.sh
+++ deply.sh
@@ -2,6 +2,6 @@
 
 cp -r ./src/main/webapp/* $CATALINA_HOME/webapps/practice/
 cp -r ./target/* $CATALINA_HOME/webapps/practice/WEB-INF/
-cp -r ./lib $CATALINA_HOME/webapps/WEB-INF/
+cp -r lib $CATALINA_HOME/webapps/practice/WEB-INF/
 
 $CATALINA_HOME/bin/startup.sh
