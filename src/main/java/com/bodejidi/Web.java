cc3d034b4b77588c494048d75b6232fba10013b8
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-16 12:25:31 +0800

    添加了执行完成调用jsp页面的代码

--- src/main/java/com/bodejidi/Web.java
+++ src/main/java/com/bodejidi/Web.java
@@ -3,6 +3,9 @@ package com.bodejidi;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 public class Web extends HttpServlet
 {
@@ -11,11 +14,15 @@ public class Web extends HttpServlet
 	
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
+		System.out.println("web");
 		Constants cons = new Constants();
 		MemberDao member = new MemberDao();
 		
-		member.userSave(cons.doParma(req));		
+		member.userSave(cons.doParma(req));
+		getServletContext()
+			.getRequestDispatcher("/jsp/registerSuccess.jsp")
+			.forward(req, resp);		
 	}
 }
