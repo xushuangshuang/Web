036a8538b03c6ec37fe0db6dc2e1f8a9ce55a1ee
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-17 18:40:05 +0800

    增加了登录检查并且对登录功能进行统一的调用

--- src/main/java/com/bodejidi/MemberService.java
+++ src/main/java/com/bodejidi/MemberService.java
@@ -10,4 +10,22 @@ public class MemberService
 		memberDao.userSave(member);
 		
 	}
+	public Boolean loadQuery(String username, String password)
+	{
+		MemberDao memberDao = new MemberDao();
+		Member member = memberDao.getMemberByUsername(username);
+		String paraUsername = member.getUsername();
+		String paraPassword = member.getPassword();
+		System.out.println(paraUsername);
+		System.out.println(paraPassword);
+		if(username.equals(paraUsername) && password.equals(paraPassword))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
 }
