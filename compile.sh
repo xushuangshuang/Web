e0ea836880c034b58888719aa0bd3d14b98d6f01
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-17 11:17:42 +0800

    增加了日志的api文件地址

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-javac -encoding "utf-8" -classpath ./lib/servlet-api.jar -d ./target/classes/ ./src/main/java/com/bodejidi/*.java
+javac -encoding "utf-8" -classpath ./lib/servlet-api.jar:./lib/slf4j-api-1.7.6.jar -d ./target/classes/ ./src/main/java/com/bodejidi/*.java
+
