e13342a02eca6175752c83f39a3e3e1966136ebe
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-17 15:26:14 +0800

    修改了对方法的调用过程

--- src/main/java/com/bodejidi/Web.java
+++ src/main/java/com/bodejidi/Web.java
@@ -3,9 +3,9 @@ package com.bodejidi;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 
 public class Web extends HttpServlet
 {
@@ -15,14 +15,12 @@ public class Web extends HttpServlet
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-	{
-	
-		Constants cons = new Constants();
-		MemberDao member = new MemberDao();
+	{ 
 		
-		member.userSave(cons.doParma(req));
+		MemberService memberService = new MemberService();
+		memberService.save(Constants.doParma(req));	
 		getServletContext()
 			.getRequestDispatcher("/jsp/registerSuccess.jsp")
-			.forward(req, resp);		
+			.forward(req, resp);	
 	}
 }
