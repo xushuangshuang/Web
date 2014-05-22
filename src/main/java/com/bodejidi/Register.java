4855283faade2bd1fff717424bac4004d682a35c
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-22 11:05:30 +0800

    添加了Register的servlet显示，用于显示登陆后的页面和信息

---
+++ src/main/java/com/bodejidi/Register.java
@@ -0,0 +1,19 @@
+package com.bodejidi;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpservletResponse;
+
+public class Register extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	{
+	
+	}
+	public void doPost(HttpServletRequest req, HttpServletresponse resp)
+	{
+		
+	
+	}
+
+}
