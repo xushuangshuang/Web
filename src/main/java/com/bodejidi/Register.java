0cf50492b960c4e6a168c129b280d47f93d4d7c4
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-23 06:33:50 +0800

    新的类处理注册页面的信息处理

--- src/main/java/com/bodejidi/Register.java
+++ src/main/java/com/bodejidi/Register.java
@@ -2,17 +2,23 @@ package com.bodejidi;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpservletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.ServletException;
 
 public class Register extends HttpServlet
 {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
-	
+		getServletContext()
+			.getRequestDispatcher("/servlet/register.jsp")
+			.forward(req, resp);	
 	}
-	public void doPost(HttpServletRequest req, HttpServletresponse resp)
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	{
-		
+		MemberService memberService = new MemberService();
+		Member member = Constants.doParma(req);
+		memberService.save(member);
 	
 	}
 
