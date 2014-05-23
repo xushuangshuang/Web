796254a05ebf2e9d12739cc808d731923830ba5f
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-24 06:43:33 +0800

    增加了显示页面的后的判断

--- src/main/java/com/bodejidi/Display.java
+++ src/main/java/com/bodejidi/Display.java
@@ -6,9 +6,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
 
 public class Display extends HttpServlet
 {
+	static final Logger logger = LoggerFactory.getLogger(Display.class);
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
 		getServletContext()
@@ -18,6 +20,14 @@ public class Display extends HttpServlet
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	{
-	
+		try
+		{
+			req.getSession().removeAttribute("username");
+			resp.sendRedirect("/practice/display");
+		}
+		catch(Exception e)
+		{
+			logger.debug("sendRedirect Exception");	
+		}
 	}
 }
