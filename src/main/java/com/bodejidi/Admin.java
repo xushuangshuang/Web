61e558e90cc7df07a1b10029685cfbf672d215b1
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-28 08:21:42 +0800

    添加了后台管理的servlet

---
+++ src/main/java/com/bodejidi/Admin.java
@@ -0,0 +1,17 @@
+package com.bodejidi;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Admin extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	{
+	
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	{
+	
+	}
+}
