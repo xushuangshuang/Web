a7db412d11b7580cac2fe77e437e07f3349639a7
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-28 21:53:07 +0800

    增加了管理员页面的底层处理

--- src/main/java/com/bodejidi/MemberDao.java
+++ src/main/java/com/bodejidi/MemberDao.java
@@ -8,36 +8,53 @@ public class MemberDao
 	static final Logger logger = LoggerFactory.getLogger(MemberDao.class);
 	String username_sql = "SELECT * FROM tb_personalInformation WHERE username = ? ";
 	String admin_sql = "SELECT * FROM administrator WHERE username = ? ";
-	public void userSave(Member member)
+	String tb_username_sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
+	String tb_personalInformation_sql = "INSERT INTO tb_personalInformation (username, password, phone, firstName, lastName,  address) VALUE(?, ?, ?, ?, ?, ?)";
+	String admin_register_sql = "INSERT INTO administrator (username, password, workcode, loginDate, workAddress, position) VALUE(?, ?, ?, ?, ?, ?)";
+	public void userSave(Member member, String workcode)
 	{
-	
+		Long code = (Long)Long.valueOf(workcode);
 		String Email = member.getEmail();
 		String username = member.getUsername();
 		String password = member.getPassword();
 		String firstName = member.getFirstName();
 		String lastName = member.getLastName();
+		String position = member.getPosition();
+		String workAddress = member.getWorkAddress(); 
 		Long phone = Long.valueOf(member.getPhone());
 		String address = member.getAddress();
-		String tb_username_sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
-		String tb_personalInformation_sql = "INSERT INTO tb_personalInformation (username, password, phone, firstName, lastName,  address) VALUE(?, ?, ?, ?, ?, ?)";
 		DatabaseService bs = DatabaseService.newInstance();
 		try
 		{
-			bs.prepare(tb_username_sql)
-				.setString(username)
-				.setString(Email)
-				.execute();
-			System.out.println("SQL: " + tb_username_sql );
-			bs.prepare(tb_personalInformation_sql)
-				.setString(username)
-				.setString(password)
-				.setLong(phone)
-				.setString(firstName)
-				.setString(lastName)
-				.setString(address)
-				.execute();
-			System.out.println("SQL: " + tb_personalInformation_sql );
-		
+			if(workcode == null)
+			{	
+				bs.prepare(tb_username_sql)
+					.setString(username)
+					.setString(Email)
+					.execute();
+				System.out.println("SQL: " + tb_username_sql );
+				bs.prepare(tb_personalInformation_sql)
+					.setString(username)
+					.setString(password)
+					.setLong(phone)
+					.setString(firstName)
+					.setString(lastName)
+					.setString(address)
+					.execute();
+				System.out.println("SQL: " + tb_personalInformation_sql );
+			}
+			else
+			{
+				bs.prepare(admin_register_sql)
+					.setString(username)
+					.setString(password)
+					.setLong(code)
+					.setDate()
+					.setString(workAddress)
+					.setString(position)
+					.execute();
+				System.out.println("SQL: " + admin_sql);
+			}
 		}
 		catch(Exception e)
 		{
