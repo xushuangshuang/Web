a46113ea37e587d8f8d68f1ec4b5c23abdaa5b6c
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-28 09:09:20 +0800

    添加页面中的doget中的页面跳转

--- src/main/java/com/bodejidi/Admin.java
+++ src/main/java/com/bodejidi/Admin.java
@@ -3,12 +3,17 @@ package com.bodejidi;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 public class Admin extends HttpServlet
 {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
-	
+		getServletContext()
+			.getRequestDispatcher("/admin/admin.jsp")
+			.forward(req, resp);
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
