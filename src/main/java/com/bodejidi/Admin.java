04ea274b7bc6f10075bce5816722f1cb648aa67e
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-28 09:36:53 +0800

    为admin增加session，当登录这个管理员的登录页面时我会给你一个session，这样在以后的动作中，我就能判断你是否为admin

--- src/main/java/com/bodejidi/Admin.java
+++ src/main/java/com/bodejidi/Admin.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import javax.servlet.http.HttpSession;
 
 public class Admin extends HttpServlet
 {
@@ -14,10 +15,34 @@ public class Admin extends HttpServlet
 		getServletContext()
 			.getRequestDispatcher("/admin/admin.jsp")
 			.forward(req, resp);
+			req.getSession().setAttribute("admin", "admin");
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
-	
+		MemberService memberService = new MemberService();
+		Member member = Constants.doParma(req);
+		String action = member.getAction();
+		System.out.println(action);
+		String username = member.getUsername();
+		String password = member.getPassword();
+		if("登录".equals(action))
+		{
+			Boolean  boo = memberService.loadQuery(username , password);
+				if(boo == true)
+				{
+					HttpSession session = req.getSession();
+					req.getSession().setAttribute("username", username);
+					getServletContext()
+						.getRequestDispatcher("/jsp/loadSuccess.jsp")
+						.forward(req, resp);
+				}
+				else
+				{
+					getServletContext()
+						.getRequestDispatcher("/jsp/loadFailed.jsp")
+						.forward(req, resp);
+				}
+		}	
 	}
 }
