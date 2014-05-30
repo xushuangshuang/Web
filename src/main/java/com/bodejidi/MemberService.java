11f84084b69c94b9dcd58b84d2cc50c6181905fb
xushuangshuang <xushuangshuang1@gmail.com>
2014-05-30 18:07:07 +0800

    在这个service类中调用DAO中的方法，完成查询的调用

--- src/main/java/com/bodejidi/MemberService.java
+++ src/main/java/com/bodejidi/MemberService.java
@@ -8,6 +8,13 @@ public class MemberService
 		MemberDao memberDao = new MemberDao();
 		memberDao.userSave(member, workcode);
 	}
+	public Member inquireUsername(String username)
+	{
+	
+		MemberDao getInformation = new MemberDao();
+		getInformation.getUsernameInformation(username);
+		return member;	
+	}
 	public Boolean loadQuery(String username, String password, String admin)
 	{
 		MemberDao memberDao = new MemberDao();
