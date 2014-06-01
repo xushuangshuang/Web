c98d4035890dc585fecc4f42ed7800e86b9ebc21
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-01 22:21:32 +0800

    由于刚才重构时调用方法出现了错误，所以 导致长度不同无法 调用故股正

--- src/main/java/com/bodejidi/MemberService.java
+++ src/main/java/com/bodejidi/MemberService.java
@@ -15,14 +15,15 @@ public class MemberService
 	{
 		String tb_username_sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
 		String tb_personalInformation_sql = "INSERT INTO tb_personalInformation (username, password, phone, firstName, lastName,  address) VALUE(?, ?, ?, ?, ?, ?)";
+		String admin_register_sql = "INSERT INTO administrator (username, password, workcode, loginDate, workAddress, position) VALUE(?, ?, ?, ?, ?, ?)";
 		System.out.println("MemberService  " + workcode);
 		MemberDao memberDao = new MemberDao();
-		memberDao.userSave(member, workcode, tb_username_sql, tb_personalInformation_sql);
+		memberDao.userSave(member, workcode, tb_username_sql, tb_personalInformation_sql, admin_register_sql);
 	}
 	public Member inquireUsername(String username)
 	{
 		String user_inquiry_username = "SELECT * FROM tb_personalInformation WHERE username = ?";
-		String admin_register_sql = "INSERT INTO administrator (username, password, workcode, loginDate, workAddress, position) VALUE(?, ?, ?, ?, ?, ?)";	
+			
 		MemberDao getInformation = new MemberDao();
 		Member member =	getInformation.getUsernameInformation(username, user_inquiry_username, admin_register_sql);
 		return member;	
