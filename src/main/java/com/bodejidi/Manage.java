f21b625cdb86680bb19ea095b50f1a32a2ca018b
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-01 11:19:51 +0800

    增加了为新类导入了get方法并且跳转到相应的页面

--- src/main/java/com/bodejidi/Manage.java
+++ src/main/java/com/bodejidi/Manage.java
@@ -1,27 +1,32 @@
 package com.bodejidi;
 
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequset;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 
 public class Manage extends HttpServlet
 {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
-	
+		getServletContext()
+			.getRequestDispatcher("/admin/adminManage.jsp")
+			.forward(req, resp);
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	{
 		MemberService memberService = new MemberService();
-		Member memberJsp = Constants.doPatma(req);
+		Member memberJsp = Constants.doParma(req);
 		String action = memberJsp.getAction();
 		String username = memberJsp.getUsername();
 		System.out.println(action);
 		if("查询".equals(action))
 		{
-			Member member = memerService.inquireUsername(username);
+			Member member = memberService.inquireUsername(username);
 			req.getSession().setAttribute("member", member);
 		}
 
