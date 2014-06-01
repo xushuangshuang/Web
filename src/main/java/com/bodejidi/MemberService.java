f6bf9f80b20ecd0e097371ec79cd7f812b7814d6
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-01 22:00:59 +0800

    将修改后的方法使用到相应的方法中完善service方法

--- src/main/java/com/bodejidi/MemberService.java
+++ src/main/java/com/bodejidi/MemberService.java
@@ -6,21 +6,24 @@ public class MemberService
 {
 	public List<Member> showList()
 	{
+		String show_user_list = "SELECT * FROM  tb_personalInformation";
 		MemberDao memberDao = new MemberDao();
-		List<Member> member = memberDao.showList();
+		List<Member> member = memberDao.showList(show_user_list);
 		return member;	
 	}
 	public void save(Member member, String workcode)
 	{
+		String tb_username_sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
+		String tb_personalInformation_sql = "INSERT INTO tb_personalInformation (username, password, phone, firstName, lastName,  address) VALUE(?, ?, ?, ?, ?, ?)";
 		System.out.println("MemberService  " + workcode);
 		MemberDao memberDao = new MemberDao();
-		memberDao.userSave(member, workcode);
+		memberDao.userSave(member, workcode, tb_username_sql, tb_personalInformation_sql);
 	}
 	public Member inquireUsername(String username)
 	{
-	
+		String user_inquiry_username = "SELECT * FROM tb_personalInformation WHERE username = ?";	
 		MemberDao getInformation = new MemberDao();
-		Member member =	getInformation.getUsernameInformation(username);
+		Member member =	getInformation.getUsernameInformation(username, user_inquiry_username);
 		return member;	
 	}
 	public Boolean loadQuery(String username, String password, String admin)
