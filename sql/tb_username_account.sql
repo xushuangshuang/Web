8a762964909f2a68d3fba631d2697baa4963e43b
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-02 09:34:19 +0800

    增加了一个表，用于显示用户的状态和余额

---
+++ sql/tb_username_account.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `tb_username_account` (
+  `username` varchar(20) NOT NULL,
+  `endAccounts` int(11) DEFAULT NULL,
+  `atatic` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 PACK_KEYS=0;
+
