ade53346dbbe3e90a0f130f97b4110ef7a4011db
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-02 10:44:54 +0800

    增加了一条 sql语句，让新的 表添加信息

--- src/main/java/com/bodejidi/MemberService.java
+++ src/main/java/com/bodejidi/MemberService.java
@@ -16,9 +16,10 @@ public class MemberService
 		String tb_username_sql = "INSERT INTO tb_username (username, userEmail) VALUE(?, ?) ";
 		String tb_personalInformation_sql = "INSERT INTO tb_personalInformation (username, password, phone, firstName, lastName,  address) VALUE(?, ?, ?, ?, ?, ?)";
 		String admin_register_sql = "INSERT INTO administrator (username, password, workcode, loginDate, workAddress, position) VALUE(?, ?, ?, ?, ?, ?)";
+		String tb_user_accounts = "INSERT INTO tb_username_account (username) VALUE(?)";
 		System.out.println("MemberService  " + workcode);
 		MemberDao memberDao = new MemberDao();
-		memberDao.userSave(member, workcode, tb_username_sql, tb_personalInformation_sql, admin_register_sql);
+		memberDao.userSave(member, workcode, tb_username_sql, tb_personalInformation_sql, admin_register_sql, tb_user_accounts);
 	}
 	public Member inquireUsername(String username)
 	{
