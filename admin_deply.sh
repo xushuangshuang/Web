2ae1ecbfdf31dd2d1df12257f909c689d756731b
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-02 10:40:02 +0800

    修改了部署脚本，这个是管理员的部署脚本

---
+++ admin_deply.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cp -r ./src/main/webapp/* $CATALINA_HOME/webapps/practice/
+cp -r ./target/* $CATALINA_HOME/webapps/practice/WEB-INF/
+cp -r lib $CATALINA_HOME/webapps/practice/WEB-INF/
+
+$CATALINA_HOME/bin/shutdown.sh
+$CATALINA_HOME/bin/startup.sh
+
+start chrome localhost:28080/practice/admin
+
