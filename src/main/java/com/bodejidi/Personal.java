2714eed07cc106cc47a0cf5945a6e932d2840c52
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-03 21:01:22 +0800

    增加了一个新的servlet，用于处理个人信息

---
+++ src/main/java/com/bodejidi/Personal.java
@@ -0,0 +1,35 @@
+package com.bodejidi;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import javax.servlet.http.HttpSession;
+
+public class Personal extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		String username = (String)req.getSession().getAttribute("username");
+		if(username != null)
+		{
+			getServletContext()
+				.getRequestDispatcher("/jsp/personal.jsp")
+				.forward(req, resp);
+		}
+		else
+		{
+			getServletContext()
+				.getRequestDispatcher("/error/noLoad.jsp")
+				.forward(req, resp);
+		
+		}
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+
+	}
+}	
