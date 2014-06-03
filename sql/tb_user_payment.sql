e64256554847750f1000c3e6efce71077db9da30
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-04 06:44:37 +0800

    昨天才发现这个sql语句出现了错误，很是惭愧

--- sql/tb_user_payment.sql
+++ sql/tb_user_payment.sql
@@ -1,11 +1,8 @@
-CREATE TABLE `administrator` (
-  `username` varchar(40) NOT NULL,
-  `password` varchar(240) NOT NULL,
-  `workcode` int(120) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `loginDate` date DEFAULT NULL,
-  `workAddress` varchar(120) NOT NULL,
-  `position` varchar(120) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 PACK_KEYS=0;
+CREATE TABLE `tb_user_payment` (
+  `username` varchar(20) DEFAULT NULL,
+  `VIP` char(20) DEFAULT NULL,
+  `accounted` varchar(20) DEFAULT NULL,
+  `payment` varchar(20) DEFAULT NULL,
+  `paymentDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 PACK_KEYS=0;
 
