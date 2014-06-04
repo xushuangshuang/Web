8ca2a5e30070bee6860a5242e327fafcd5814f17
xushuangshuang <xushuangshuang1@gmail.com>
2014-06-04 11:27:55 +0800

    增加了一个是否登陆的判断

--- src/main/java/com/bodejidi/Manage.java
+++ src/main/java/com/bodejidi/Manage.java
@@ -8,44 +8,55 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 
 public class Manage extends HttpServlet
 {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
-		MemberService memberService = new MemberService();
-		List<Member> memberList = memberService.showList();
-		HttpSession session = req.getSession();
-		System.out.println("memberList " + memberList);
-		req.getSession().setAttribute("member", memberList);
-		Member member = Constants.doParma(req);
-		String action = member.getAction();
-		System.out.println("Manage " + action);
-		String number = member.getNumber();
-		if(action != null)
+		String adminUsername = (String)req.getSession().getAttribute("adminUsername");
+		if(null == adminUsername)
 		{
-			MemberService ms = new MemberService();
-			Member parm_member = ms.informationByUsername(action);
-			req.getSession().setAttribute("informationList", parm_member);
-			if(number != null)
+			getServletContext()
+				.getRequestDispatcher("/error/404.jsp")
+				.forward(req, resp);
+		}
+		else
+		{
+			MemberService memberService = new MemberService();
+			List<Member> memberList = memberService.showList();
+			HttpSession session = req.getSession();
+			System.out.println("memberList " + memberList);
+			req.getSession().setAttribute("member", memberList);
+			Member member = Constants.doParma(req);
+			String action = member.getAction();
+			System.out.println("Manage " + action);
+			String number = member.getNumber();
+			if(action != null)
 			{
-				getServletContext()
-					.getRequestDispatcher("/admin/accounts.jsp")
-					.forward(req, resp);
+				MemberService ms = new MemberService();
+				Member parm_member = ms.informationByUsername(action);
+				req.getSession().setAttribute("informationList", parm_member);
+				if(number != null)
+				{
+					getServletContext()
+						.getRequestDispatcher("/admin/accounts.jsp")
+						.forward(req, resp);
+				}
+				else
+				{
+					getServletContext()
+						.getRequestDispatcher("/admin/adminManage.jsp")
+						.forward(req, resp);
+				}
 			}
 			else
 			{
 				getServletContext()
-					.getRequestDispatcher("/admin/adminManage.jsp")
+					.getRequestDispatcher("/admin/manageIndex.jsp")
 					.forward(req, resp);
-			}
-		}
-		else
-		{
-			getServletContext()
-				.getRequestDispatcher("/admin/manageIndex.jsp")
-				.forward(req, resp);
 		
+			}
 		}
 	}
 
